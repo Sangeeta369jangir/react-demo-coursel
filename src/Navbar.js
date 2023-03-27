172e29ceedcf50dcabbfef9ddf9dc89b0c31e738
Sangeeta <connectsonu369@gmail.com>
2023-03-28 00:00:23 +0530

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+//import { GiHamburgerMenu } from 'react-icons/gi';
+
+
+import './Navbar.css';
+
+const Navbar = () => (
+    <nav className="app__navbar">
+        <div className="app__navbar-logo">
+            <img src="./img/logo.png" alt="app logo" />
+        </div>
+    <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a> </li>
+        <li className="p__opensans"><a href="#about">About</a> </li>
+        <li className="p__opensans"><a href="#places">Places</a> </li>
+        <li className="p__opensans"><a href="#feedback">Feedback</a> </li>
+    </ul>
+
+    <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Login / Register</a>
+        <div />
+
+        <a href="/" className="p__opensans">Add Your Paradise</a>
+    </div>
+    </nav>
+);
+
+export default Navbar;
---
+++ src/image.js
@@ -0,0 +1,20 @@
+import Image1 from './img/1.png';
+
+const images = [{
+    id: 1,
+    src: Image1,
+    alt: "Image 1"
+   
+},
+{
+    id: 2,
+    src: "./img/2.png",
+    alt: "Image 2 "
+},
+{
+    id: 3,
+    src: "./img/3.png",
+    alt: "Image 3"
+}
+];
+export default images;
